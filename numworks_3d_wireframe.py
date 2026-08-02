328d75379d9816d78d9e2e4467da503cd789b7e3
Anton Simmerle <anton@simmerle.com>
2026-08-02 11:16:04 +0200

    feat: add interactive rotation and refactor rendering

--- numworks_3d_wireframe.py
+++ numworks_3d_wireframe.py
@@ -1,6 +1,7 @@
 from kandinsky import color, fill_rect, set_pixel
 from math import sin, cos
 from time import sleep
+from ion import *
 
 X_MAX = 320
 Y_MAX = 222
@@ -10,19 +11,27 @@ Y_CEN = Y_MAX // 2
 BLACK = color(0, 0, 0)
 WHITE = color(255, 255, 255)
 
-SCALE = 100
+angx = 0
+angy = 0
+#angz = 0
 FRAME_TIME = 1/30
-ang = 0
+ROT_SPEED = 0.3
+
+SCALE = 100
 
 VERTS = [
-  (-0.5, -0.5, 0.5),  #a
-  (0.5,  -0.5, 0.5),  #b
-  (0.5,  -0.5, -0.5), #c
-  (-0.5, -0.5, -0.5), #d
-  (-0.5, 0.5, 0.5),   #e
-  (0.5,  0.5, 0.5),   #f
-  (0.5,  0.5, -0.5),  #g
-  (-0.5, 0.5, -0.5),  #h
+  (-0.5, -0.5, 0.5),
+  (0.5, -0.5, 0.5),
+  (0.5, -0.5, -0.5),
+  (-0.5, -0.5, -0.5),
+  (-0.5, 0.5, 0.5),
+  (0.5, 0.5, 0.5),
+  (0.5, 0.5, -0.5),
+  (-0.5, 0.5, -0.5),
+  (-0.2, 0, 0.2),
+  (0.2, 0, 0.2),
+  (0.2, 0, -0.2),
+  (-0.2, 0, -0.2),
 ]
 
 EDGES = [
@@ -30,30 +39,28 @@ EDGES = [
   (1, 2),
   (2, 3),
   (3, 0),
-  
   (4, 5),
   (5, 6),
   (6, 7),
   (7, 4),
-
-  (0, 4),
-  (1, 5),
-  (2, 6),
-  (3, 7),
+  (8, 9),
+  (9, 10),
+  (10, 11),
+  (11, 8),
+  (0, 8),
+  (1, 9),
+  (2, 10),
+  (3, 11),
+  (4, 8),
+  (5, 9),
+  (6, 10),
+  (7, 11),
 ]
 
-def r_to_a(p):
-  x, y = p
-  return (int(x*SCALE + X_CEN),
-          int(Y_CEN - y*SCALE))
-
 # TODO: draw_line returns a set of pixels
-def draw_line(r1, r2):
-  ax1, ay1 = r_to_a(r1)
-  ax2, ay2 = r_to_a(r2)
-  
-  dx = ax2 - ax1
-  dy = ay2 - ay1
+def draw_line(x1, y1, x2, y2):  
+  dx = x2 - x1
+  dy = y2 - y1
   x_dir = 1 if dx >= 0 else -1
   y_dir = 1 if dy >= 0 else -1
   dx = abs(dx)
@@ -67,54 +74,84 @@ def draw_line(r1, r2):
   D = 2*dy - dx
   y = 0
   for x in range(dx + 1):
-    set_pixel(ax1 + x*prim_x + y*sec_x,
-              ay1 + x*prim_y + y*sec_y, BLACK)
+    set_pixel(x1 + x*prim_x + y*sec_x,
+              y1 + x*prim_y + y*sec_y, BLACK)
     if D < 0:
       D = D + 2*dy
     else:
       y += 1
       D = D + 2*(dy - dx)
 
-def mat_vec(mat, vec):
-  return (vec[0]*mat[0][0] + vec[1]*mat[0][1] + vec[2]*mat[0][2],
-          vec[0]*mat[1][0] + vec[1]*mat[1][1] + vec[2]*mat[1][2],
-          vec[0]*mat[2][0] + vec[1]*mat[2][1] + vec[2]*mat[2][2])
+# TODO: make rotation world axis for x and y
+def rotate_vert(p, sx, cx, sy, cy):
+  x, y, z = p
+  # Rotate X
+  y1 = y*cx - z*sx
+  z1 = y*sx + z*cx
+  
+  # Rotate Y
+  x1 = x*cy + z1*sy
+  z1 = x*(-sy) + z1*cy
+  
+  return (x1, y1, z1)
 
+def proj_vert(p):
+  x, y, z = p
+  return (int(x*SCALE + X_CEN),
+          int(Y_CEN - y*SCALE))
+  
 def draw():
-  global ang
-
   # TODO: draw only changed pixels from the previous draw using sets
   fill_rect(0, 0, X_MAX, Y_MAX, WHITE)
   
-  s = sin(ang)
-  c = cos(ang)
-  Rx = [( 1,  0,  0),
-        ( 0,  c, -s),
-        ( 0,  s,  c)]
-  Ry = [( c,  0,  s),
-        ( 0,  1,  0),
-        (-s,  0,  c)]
-  Rz = [( c, -s,  0),
-        ( s,  c,  0),
-        ( 0,  0,  1)]
+  sx = sin(angx)
+  cx = cos(angx)
+  sy = sin(angy)
+  cy = cos(angy)
+
+  # TODO: use fixed size arrays
+  rot_verts = [
+    rotate_vert(p, sx, cx, sy, cy)
+    for p in VERTS
+  ]
+  proj_verts = [
+    proj_vert(p)
+    for p in rot_verts
+  ]
   
-  rot_verts = []
-  for p in VERTS:
-    rot_p = mat_vec(Rx, p)
-    rot_p = mat_vec(Ry, rot_p)
-    rot_p = mat_vec(Rz, rot_p)
-    rot_verts.append(rot_p)
-
   for a, b in EDGES:
-    x1, y1, z1 = rot_verts[a]
-    x2, y2, z2 = rot_verts[b]
-    draw_line((x1, y1), (x2, y2))
+    draw_line(proj_verts[a][0],
+              proj_verts[a][1],
+              proj_verts[b][0],
+              proj_verts[b][1])
 
-  ang += 0.1
+def update():
+  global angx, angy, ROT_SPEED
+  updated = False
+  # TODO: add deltatime rotation and radians per second
+  # TODO: normalize angles if they exeed 2Pi
+  if (keydown(KEY_UP)):
+    angx -= ROT_SPEED
+    updated = True
+  if (keydown(KEY_DOWN)):
+    angx += ROT_SPEED
+    updated = True
+  if (keydown(KEY_LEFT)):
+    angy -= ROT_SPEED
+    updated = True
+  if (keydown(KEY_RIGHT)):
+    angy += ROT_SPEED
+    updated = True
+
+  return updated  
 
 def main():
-  for _ in range(100):
-    draw()
+  draw()
+  for _ in range(500):
+    if (update()):
+      draw()
+    # TODO: sleep the actual time difference
+    # TODO: Manage overshooting the deadline
     sleep(FRAME_TIME)
 
 main()
