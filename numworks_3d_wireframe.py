d1827f3e56f25fc55e27755655ee3e14dd1d0a6d
Anton Simmerle <anton@simmerle.com>
2026-08-03 22:29:39 +0200

    feat: add perspective projection

--- numworks_3d_wireframe.py
+++ numworks_3d_wireframe.py
@@ -1,23 +1,22 @@
-from kandinsky import color, fill_rect, set_pixel
+from kandinsky import *
 from math import sin, cos
 from time import sleep
 from ion import *
 
-X_MAX = 320
-Y_MAX = 222
-X_CEN = X_MAX // 2
-Y_CEN = Y_MAX // 2
+CAN_W = 320
+CAN_H = 222
+CAN_CEN_X = CAN_W // 2
+CAN_CEN_Y = CAN_H // 2
 
 BLACK = color(0, 0, 0)
 WHITE = color(255, 255, 255)
 
-angx = 0
-angy = 0
-#angz = 0
-FRAME_TIME = 1/30
-ROT_SPEED = 0.3
-
-SCALE = 100
+rot_x = 0
+rot_y = 0
+sx = 0.0
+cx = 1.0
+sy = 0.0
+cy = 1.0
 
 VERTS = [
   (-0.5, -0.5, 0.5),
@@ -57,101 +56,100 @@ EDGES = [
   (7, 11),
 ]
 
-# TODO: draw_line returns a set of pixels
-def draw_line(x1, y1, x2, y2):  
-  dx = x2 - x1
-  dy = y2 - y1
+proj_verts = [[0, 0] for _ in VERTS]
+
+VP_W = 2.0
+VP_H = VP_W * CAN_H / CAN_W
+CAM_Z = 2
+FOCAL_LEN = 1
+
+def draw_line(x0, y0, x1, y1, col):  
+  dx = x1 - x0
+  dy = y1 - y0
   x_dir = 1 if dx >= 0 else -1
   y_dir = 1 if dy >= 0 else -1
   dx = abs(dx)
   dy = abs(dy)
+  
   if dx > dy:
     prim_x, prim_y, sec_x, sec_y = x_dir, 0, 0, y_dir
   else:
     dx, dy = dy, dx
     prim_x, prim_y, sec_x, sec_y = 0, y_dir, x_dir, 0
-  
+
   D = 2*dy - dx
-  y = 0
-  for x in range(dx + 1):
-    set_pixel(x1 + x*prim_x + y*sec_x,
-              y1 + x*prim_y + y*sec_y, BLACK)
-    if D < 0:
-      D = D + 2*dy
-    else:
-      y += 1
+  x = x0
+  y = y0
+  for _ in range(dx + 1):
+    set_pixel(x, y, col)
+    x += prim_x
+    y += prim_y
+    
+    if D >= 0:
+      x += sec_x
+      y += sec_y
       D = D + 2*(dy - dx)
+    else:
+      D = D + 2*dy
 
-# TODO: make rotation world axis for x and y
-def rotate_vert(p, sx, cx, sy, cy):
-  x, y, z = p
-  # Rotate X
-  y1 = y*cx - z*sx
-  z1 = y*sx + z*cx
-  
-  # Rotate Y
-  x1 = x*cy + z1*sy
-  z1 = x*(-sy) + z1*cy
-  
-  return (x1, y1, z1)
-
-def proj_vert(p):
-  x, y, z = p
-  return (int(x*SCALE + X_CEN),
-          int(Y_CEN - y*SCALE))
-  
 def draw():
-  # TODO: draw only changed pixels from the previous draw using sets
-  fill_rect(0, 0, X_MAX, Y_MAX, WHITE)
+  # TODO: erase only lines, not full screen
+  fill_rect(0, 0, CAN_W, CAN_H, WHITE)
   
-  sx = sin(angx)
-  cx = cos(angx)
-  sy = sin(angy)
-  cy = cos(angy)
+  for i, p in enumerate(VERTS):
+    x, y, z = p
+    
+    # Rotate around X
+    r_xx = x
+    r_xy = y * cx - z * sx
+    r_xz = y * sx + z * cx
+    # Rotate around Y
+    r_yx = r_xx * cy + r_xz * sy
+    r_yy = r_xy
+    r_yz = -r_xx * sy + r_xz * cy
 
-  # TODO: use fixed size arrays
-  rot_verts = [
-    rotate_vert(p, sx, cx, sy, cy)
-    for p in VERTS
-  ]
-  proj_verts = [
-    proj_vert(p)
-    for p in rot_verts
-  ]
-  
+    # Project
+    # TODO: add near plane clipping
+    depth = r_yz + CAM_Z
+    proj_x = (r_yx * FOCAL_LEN * CAN_W) / (VP_W * depth)
+    proj_y = (r_yy * FOCAL_LEN * CAN_H) / (VP_H * depth)
+    
+    p = proj_verts[i]
+    p[0] = int(proj_x + CAN_CEN_X)
+    p[1] = int(CAN_CEN_Y - proj_y)
+
+  # Draw
   for a, b in EDGES:
-    draw_line(proj_verts[a][0],
-              proj_verts[a][1],
-              proj_verts[b][0],
-              proj_verts[b][1])
+    pa = proj_verts[a]
+    pb = proj_verts[b]
+    draw_line(pa[0], pa[1], pb[0], pb[1], BLACK)
 
 def update():
-  global angx, angy, ROT_SPEED
-  updated = False
-  # TODO: add deltatime rotation and radians per second
-  # TODO: normalize angles if they exeed 2Pi
-  if (keydown(KEY_UP)):
-    angx -= ROT_SPEED
-    updated = True
-  if (keydown(KEY_DOWN)):
-    angx += ROT_SPEED
-    updated = True
-  if (keydown(KEY_LEFT)):
-    angy -= ROT_SPEED
-    updated = True
-  if (keydown(KEY_RIGHT)):
-    angy += ROT_SPEED
-    updated = True
+  global rot_x, rot_y, sx, cx, sy, cy
 
-  return updated  
+  rot_dir_x = keydown(KEY_DOWN) - keydown(KEY_UP)
+  rot_dir_y = keydown(KEY_RIGHT) - keydown(KEY_LEFT)
+
+  if rot_dir_x == 0 and rot_dir_y == 0:
+    return False
+  if rot_dir_x:
+    rot_x += rot_dir_x * 0.3
+    sx = sin(rot_x)
+    cx = cos(rot_x)
+  if rot_dir_y:
+    rot_y += rot_dir_y * 0.3
+    sy = sin(rot_y)
+    cy = cos(rot_y)
+
+  return True
 
 def main():
   draw()
-  for _ in range(500):
-    if (update()):
+  while True:
+    if update():
       draw()
     # TODO: sleep the actual time difference
     # TODO: Manage overshooting the deadline
-    sleep(FRAME_TIME)
+    sleep(1/30)
 
 main()
