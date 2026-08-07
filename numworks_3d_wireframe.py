164afbf4c82767db81b38d4ca243ba5d2a145f1a
Anton Simmerle <anton@simmerle.com>
2026-08-07 11:42:29 +0200

    feat: add near-plane clipping

--- numworks_3d_wireframe.py
+++ numworks_3d_wireframe.py
@@ -1,7 +1,9 @@
-from kandinsky import set_pixel, color, draw_string, fill_rect
+from kandinsky import set_pixel, color, fill_rect
 from math import sin, cos
-from time import sleep, monotonic
+from time import monotonic
 from ion import *
+
+# TODO: add reset button KEY_FIVE
 
 CAN_W = 320
 CAN_H = 222
@@ -16,6 +18,7 @@ VP_H = VP_W * CAN_H / CAN_W
 obj_z = 2
 FOCAL_LEN = 1
 PROJ_SCALE = (FOCAL_LEN * CAN_W) / VP_W
+NEAR = 0.05
 
 ROT_SPEED = 1.0
 MOVE_SPEED_Z = 1.0
@@ -37,10 +40,10 @@ VERTS = (
   (0.5, 0.5, 0.5),
   (0.5, 0.5, -0.5),
   (-0.5, 0.5, -0.5),
-  (-0.2, 0, 0.2),
-  (0.2, 0, 0.2),
-  (0.2, 0, -0.2),
-  (-0.2, 0, -0.2),
+  (-0.25, 0, 0.25),
+  (0.25, 0, 0.25),
+  (0.25, 0, -0.25),
+  (-0.25, 0, -0.25),
 )
 
 EDGES = (
@@ -66,9 +69,14 @@ EDGES = (
   (7, 11),
 )
 
-proj_verts = [[0, 0] for _ in VERTS]
+cam_verts = [[0, 0, 0] for _ in VERTS]
 
 def draw_line(x0, y0, x1, y1, col):
+  x0 = int(x0)
+  y0 = int(y0)
+  x1 = int(x1)
+  y1 = int(y1)
+  
   dx = abs(x1 - x0)
   dy = -abs(y1 - y0)
   sx = 1 if x0 < x1 else -1
@@ -102,48 +110,69 @@ def draw():
     r_yy = r_xy
     r_yz = -r_xx * sin_y + r_xz * cos_y
 
-    # Project
-    depth = r_yz + obj_z
-    proj_x = (r_yx / depth) * PROJ_SCALE
-    proj_y = (r_yy / depth) * PROJ_SCALE
-    
-    p = proj_verts[i]
-    p[0] = int(proj_x + CAN_CEN_X)
-    p[1] = int(CAN_CEN_Y - proj_y)
+    cam_verts[i][0] = r_yx
+    cam_verts[i][1] = r_yy
+    cam_verts[i][2] = r_yz + obj_z
 
-  # Draw
+  # Near-plane clipping and drawing
   fill_rect(0, 0, CAN_W, CAN_H, WHITE)
   for a, b in EDGES:
-    pa = proj_verts[a]
-    pb = proj_verts[b]
-    draw_line(pa[0], pa[1], pb[0], pb[1], BLACK)
+    ax, ay, az = cam_verts[a]
+    bx, by, bz = cam_verts[b]
+
+    if az < NEAR and bz < NEAR:
+      continue
+
+    if az < NEAR:
+      t = (NEAR - az) / (bz - az)
+      ax = ax + t * (bx - ax)
+      ay = ay + t * (by - ay)
+      az = NEAR
+      
+    elif bz < NEAR:
+      t = (NEAR - az) / (bz - az)
+      bx = ax + t * (bx - ax)
+      by = ay + t * (by - ay)
+      bz = NEAR
+    
+    p_ax = (ax / az) * PROJ_SCALE
+    p_ay = (ay / az) * PROJ_SCALE
+    p_bx = (bx / bz) * PROJ_SCALE
+    p_by = (by / bz) * PROJ_SCALE
+  
+    c_ax = p_ax + CAN_CEN_X
+    c_ay = CAN_CEN_Y - p_ay
+    c_bx = p_bx + CAN_CEN_X
+    c_by = CAN_CEN_Y - p_by
+
+    draw_line(c_ax, c_ay, c_bx, c_by, BLACK)
 
 def update(dt):
   global rot_x, rot_y, sin_x, cos_x, sin_y, cos_y, obj_z
 
   rot_dir_x = keydown(KEY_TWO) - keydown(KEY_EIGHT)
   rot_dir_y = keydown(KEY_SIX) - keydown(KEY_FOUR)
-  
   move_dir_z = keydown(KEY_SEVEN) - keydown(KEY_NINE)
 
-  if (rot_dir_x == 0 and
-      rot_dir_y == 0 and
-      move_dir_z == 0):
-    return False
+  updated = False
 
   if rot_dir_x != 0:
     rot_x += rot_dir_x * dt * ROT_SPEED
     sin_x = sin(rot_x)
     cos_x = cos(rot_x)
+    updated = True
+
   if rot_dir_y != 0:
     rot_y += rot_dir_y * dt * ROT_SPEED
     sin_y = sin(rot_y)
     cos_y = cos(rot_y)
+    updated = True
 
   if move_dir_z != 0:
     obj_z += move_dir_z * dt * MOVE_SPEED_Z
+    updated = True
 
-  return True
+  return updated
 
 def main():
   draw()
