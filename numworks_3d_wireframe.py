e59056f822bc050f3356c690b4432d029eb2a337
Anton Simmerle <anton@simmerle.com>
2026-08-07 15:27:53 +0200

    feat: add Cohen-Sutherland line clipping and reset button

--- numworks_3d_wireframe.py
+++ numworks_3d_wireframe.py
@@ -3,28 +3,30 @@ from math import sin, cos
 from time import monotonic
 from ion import *
 
-# TODO: add reset button KEY_FIVE
-
-CAN_W = 320
-CAN_H = 222
-CAN_CEN_X = CAN_W // 2
-CAN_CEN_Y = CAN_H // 2
+SCR_W = 320
+SCR_H = 222
+SCR_CEN_X = SCR_W // 2
+SCR_CEN_Y = SCR_H // 2
 
 BLACK = color(0, 0, 0)
 WHITE = color(255, 255, 255)
 
+LEFT = 8
+RIGHT = 4
+BOTTOM = 2
+TOP = 1
+
 VP_W = 2.0
-VP_H = VP_W * CAN_H / CAN_W
-obj_z = 2
-FOCAL_LEN = 1
-PROJ_SCALE = (FOCAL_LEN * CAN_W) / VP_W
+FL = 1.0
+PROJ_S = FL * SCR_W / VP_W
 NEAR = 0.05
 
 ROT_SPEED = 1.0
-MOVE_SPEED_Z = 1.0
+Z_SPEED = 1.0
 
-rot_x = 0
-rot_y = 0
+rot_x = 0.0
+rot_y = 0.0
+obj_z = 2.0
 
 sin_x = 0.0
 cos_x = 1.0
@@ -69,9 +71,9 @@ EDGES = (
   (7, 11),
 )
 
-cam_verts = [[0, 0, 0] for _ in VERTS]
+cam_verts = [[0.0, 0.0, 0.0] for _ in VERTS]
 
-def draw_line(x0, y0, x1, y1, col):
+def bresenham(x0, y0, x1, y1):
   x0 = int(x0)
   y0 = int(y0)
   x1 = int(x1)
@@ -84,7 +86,7 @@ def draw_line(x0, y0, x1, y1, col):
   
   err = dx + dy
   while True:
-    set_pixel(x0, y0, col)
+    set_pixel(x0, y0, BLACK)
 
     if x0 == x1 and y0 == y1:
       break
@@ -97,25 +99,77 @@ def draw_line(x0, y0, x1, y1, col):
       err += dx
       y0 += sy
 
+def outcode(x, y):
+  code = 0
+  
+  if x < 0:
+    code |= LEFT
+  elif x > (SCR_W - 1):
+    code |= RIGHT
+
+  if y > SCR_H - 1:
+    code |= BOTTOM
+  elif y < 0:
+    code |= TOP
+
+  return code
+
+def cohen_sutherland(x0, y0, x1, y1):
+  while True:
+    c0 = outcode(x0, y0)
+    c1 = outcode(x1, y1)
+    
+    if (c0 | c1) == 0:
+      return x0, y0, x1, y1
+    if (c0 & c1) != 0:
+      return None
+  
+    out = c0 if c0 != 0 else c1
+
+    if out & LEFT:
+      t = (0 - x0) / (x1 - x0)
+      x = 0
+      y = y0 + t * (y1 - y0)
+
+    elif out & RIGHT:
+      t = ((SCR_W - 1) - x0) / (x1 - x0)
+      x = SCR_W - 1
+      y = y0 + t * (y1 - y0)
+
+    elif out & BOTTOM:
+      t = ((SCR_H - 1) - y0) / (y1 - y0)
+      x = x0 + t * (x1 - x0)
+      y = SCR_H - 1
+
+    elif out & TOP:
+      t = (0 - y0) / (y1 - y0)
+      x = x0 + t * (x1 - x0)
+      y = 0
+
+    if out == c0:
+      x0, y0 = x, y
+    else:
+      x1, y1 = x, y
+
 def draw():
   for i, p in enumerate(VERTS):
     x, y, z = p
     
     # Rotate around X
-    r_xx = x
-    r_xy = y * cos_x - z * sin_x
-    r_xz = y * sin_x + z * cos_x
+    x1 = x
+    y1 = y * cos_x - z * sin_x
+    z1 = y * sin_x + z * cos_x
     # Rotate around Y
-    r_yx = r_xx * cos_y + r_xz * sin_y
-    r_yy = r_xy
-    r_yz = -r_xx * sin_y + r_xz * cos_y
+    x2 = x1 * cos_y + z1 * sin_y
+    y2 = y1
+    z2 = -x1 * sin_y + z1 * cos_y
 
-    cam_verts[i][0] = r_yx
-    cam_verts[i][1] = r_yy
-    cam_verts[i][2] = r_yz + obj_z
+    cam_verts[i][0] = x2
+    cam_verts[i][1] = y2
+    cam_verts[i][2] = z2 + obj_z
 
   # Near-plane clipping and drawing
-  fill_rect(0, 0, CAN_W, CAN_H, WHITE)
+  fill_rect(0, 0, SCR_W, SCR_H, WHITE)
   for a, b in EDGES:
     ax, ay, az = cam_verts[a]
     bx, by, bz = cam_verts[b]
@@ -135,24 +189,22 @@ def draw():
       by = ay + t * (by - ay)
       bz = NEAR
     
-    p_ax = (ax / az) * PROJ_SCALE
-    p_ay = (ay / az) * PROJ_SCALE
-    p_bx = (bx / bz) * PROJ_SCALE
-    p_by = (by / bz) * PROJ_SCALE
-  
-    c_ax = p_ax + CAN_CEN_X
-    c_ay = CAN_CEN_Y - p_ay
-    c_bx = p_bx + CAN_CEN_X
-    c_by = CAN_CEN_Y - p_by
+    s_ax = SCR_CEN_X + ax / az * PROJ_S
+    s_ay = SCR_CEN_Y - ay / az * PROJ_S
+    s_bx = SCR_CEN_X + bx / bz * PROJ_S
+    s_by = SCR_CEN_Y - by / bz * PROJ_S
 
-    draw_line(c_ax, c_ay, c_bx, c_by, BLACK)
+    clipped = cohen_sutherland(s_ax, s_ay, s_bx, s_by)
+    if clipped is not None:
+      s_ax, s_ay, s_bx, s_by = clipped
+      bresenham(s_ax, s_ay, s_bx, s_by)
 
 def update(dt):
   global rot_x, rot_y, sin_x, cos_x, sin_y, cos_y, obj_z
 
-  rot_dir_x = keydown(KEY_TWO) - keydown(KEY_EIGHT)
-  rot_dir_y = keydown(KEY_SIX) - keydown(KEY_FOUR)
-  move_dir_z = keydown(KEY_SEVEN) - keydown(KEY_NINE)
+  rot_dir_x = keydown(KEY_EIGHT) - keydown(KEY_TWO)
+  rot_dir_y = keydown(KEY_FOUR) - keydown(KEY_SIX)
+  z_dir = keydown(KEY_NINE) - keydown(KEY_SEVEN)
 
   updated = False
 
@@ -168,20 +220,32 @@ def update(dt):
     cos_y = cos(rot_y)
     updated = True
 
-  if move_dir_z != 0:
-    obj_z += move_dir_z * dt * MOVE_SPEED_Z
+  if z_dir != 0:
+    obj_z += z_dir * dt * Z_SPEED
     updated = True
+
+  if keydown(KEY_FIVE):
+    rot_x = 0.0
+    rot_y = 0.0
+    obj_z = 2.0
+
+    sin_x = 0.0
+    cos_x = 1.0
+    sin_y = 0.0
+    cos_y = 1.0
+
+    return True
 
   return updated
 
 def main():
   draw()
-  prev_start = monotonic()
+  prev_t = monotonic()
   
   while True:
-    start = monotonic()
-    dt = start - prev_start
-    prev_start = start
+    t = monotonic()
+    dt = t - prev_t
+    prev_t = t
     
     if update(dt):
       draw()
